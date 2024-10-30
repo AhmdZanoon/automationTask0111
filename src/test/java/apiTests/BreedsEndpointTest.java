77324290ac487ac1c80a00df9ba7bc26aa775fc6
Ahmed Zanoon <65794900+zanoon2020@users.noreply.github.com>
2024-10-30 22:54:49 +0300

    update docker file

--- src/test/java/apiTests/BreedsEndpointTest.java
+++ src/test/java/apiTests/BreedsEndpointTest.java
@@ -5,14 +5,14 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Link;
 import io.qameta.allure.restassured.AllureRestAssured;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
-
-public class BreedsEndpointTest extends ApiTestSetup {
 
 @Epic("API Tests")
 @Feature("Breeds Endpoint")
 @Link("https://developers.thedogapi.com/")
+public class BreedsEndpointTest extends ApiTestSetup {
 
 
     @Test(description = "TC0201 - Get Breeds With Api Key Provided And Assert That Image Url Return")
@@ -21,31 +21,30 @@ public class BreedsEndpointTest extends ApiTestSetup {
                 .filter(new AllureRestAssured())
                 .header("x-api-key", apiProperties.apiKey())
                 .queryParam("limit", 5)
-                .queryParam("page",0)
+                .queryParam("page", 0)
                 .log().all().
                 when().
                 get("/breeds/").
                 then().log().all()
                 .statusCode(200)
-                .body("image.url",everyItem(notNullValue()))
+                .body("image.url", everyItem(notNullValue()))
                 .extract()
                 .response();
 
     }
-
 
     @Test(description = "TC0202 - Get Breeds Without Api Key Provided And Assert On Size Of Response and The Name Of first Breed")
     public void _02GetBreedsWithoutAuthentication() {
         given()
                 .filter(new AllureRestAssured())
                 .queryParam("limit", 2)
-                .queryParam("page",0)
+                .queryParam("page", 0)
                 .log().all().
                 when().
                 get("/breeds/").
                 then().log().all()
                 .statusCode(200)
-                .body("size()",equalTo(2))
+                .body("size()", equalTo(2))
                 .body("find { it.id == 1 }.name", equalTo("Affenpinscher"))
                 .extract()
                 .response();
